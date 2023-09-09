70272baa64aa96f4f2c0c26f263a859f992f7306
Faisal Amin <faysalamin001@gmail.com>
2023-09-09 08:33:32 +0500

    initial project structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+const { logger } = require('./utils/logger');
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+  logger.info(`Running on PORT ${PORT}`);
+});
